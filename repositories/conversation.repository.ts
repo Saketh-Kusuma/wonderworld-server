c8631b3d5d9de84d447b74a727df0249bf55915b
Saketh Kusuma <kusumasaketh92@gmail.com>
2026-01-26 22:56:16 +0530

    Added date context

--- repositories/conversation.repository.ts
+++ repositories/conversation.repository.ts
@@ -1,14 +1,17 @@
-import fs from 'fs'
+import fs from 'fs';
 import path from 'path';
-import template from '../prompts/chatbots.txt'
+import template from '../prompts/chatbots.txt';
 import type { Message } from '@openrouter/sdk/models';
 const parkInfo = fs.readFileSync(
    path.join(__dirname, '..', 'prompts', 'WonderWorld.md'),
    'utf-8'
 );
-const instructions = template.replace('{{parkInfo}}', parkInfo);
+const date = new Date().getDate();
+const instructions =
+   template.replace('{{parkInfo}}', parkInfo) +
+   template.replace('{{date}}', date.toLocaleString());
 export let messages: Message[] = [
-   { role: 'system', content: instructions},
+   { role: 'system', content: instructions },
    { role: 'user', content: 'Hello!' },
 ];
 const chatMap = new Map<string, Message[]>();
